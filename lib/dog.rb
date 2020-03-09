e7a7576a8f9b26e3c7b6935622c090f212c4d7a7
Elijah wines <elijahwines84@yahoo.com>
2020-03-09 13:16:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,17 @@
 class Dog
   @@all = []
-  attr_accessor :owner, :mood 
-  attr_reader :name 
+  attr_accessor :owner, :mood
+  attr_reader :name
 
   def initialize(name, owner)
-    @name = name 
-    @owner = owner 
+    @name = name
+    @owner = owner
     @mood = "nervous"
     @@all << self
   end
 
-  def self.all 
-    @@all 
+  def self.all
+    @@all
   end
   
 end
