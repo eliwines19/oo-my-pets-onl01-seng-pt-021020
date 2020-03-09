f77ad9ffbf3bdcc82379767fbf100d62d66a796b
Elijah wines <elijahwines84@yahoo.com>
2020-03-09 13:49:02 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -50,7 +50,7 @@ class Owner
   end
 
   def list_pets
-    puts "I have #{@owner.dogs.count} dog(s), and #{@owner.cats.count} cat(s)."
+    puts "I have #{self.dogs.length} dog(s), and #{self.cats.length} cat(s)."
   end
 
 
