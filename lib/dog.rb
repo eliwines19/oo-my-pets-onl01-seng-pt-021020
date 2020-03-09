ad0494df80499ea4141834b8d0f87dc3f41ad886
Elijah wines <elijahwines84@yahoo.com>
2020-03-09 13:57:33 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,9 +9,12 @@ class Dog
     @mood = "nervous"
     @@all << self
   end
+  #initializes the instance of a dog being created by giving it a name and owner, a set mood of nervous and pushes the dog to the
+  #all class variable array
 
   def self.all
     @@all
   end
+  #returns the instances of dogs that have been created 
   
 end
